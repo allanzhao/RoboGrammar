4511f452a905841d1888c0fa6e16ffc48e5caaa5
Allan Zhao <allanzhao@users.noreply.github.com>
2019-08-11 16:49:22 -0400

    Add knees to quadruped, remove joints at base
      - initial position is now on the ground, instead of in midair

--- src/main.cpp
+++ src/main.cpp
@@ -51,23 +51,30 @@ int main(int argc, char **argv) {
       /*joint_axis=*/Vector3{0.0, 0.0, 1.0},
       /*length=*/0.4);
   for (Index i = 0; i < 4; ++i) {
-    Quaternion joint_rot(Eigen::AngleAxis<Scalar>(0.0, Vector3::UnitZ()) *
-                         Eigen::AngleAxis<Scalar>((i - 0.5) * M_PI / 2, Vector3::UnitY()) *
-                         Eigen::AngleAxis<Scalar>(0.0, Vector3::UnitX()));
+    Quaternion leg_rot(Eigen::AngleAxis<Scalar>((i - 0.5) * M_PI / 2, Vector3::UnitY()));
+    Quaternion thigh_rot(Eigen::AngleAxis<Scalar>(M_PI / 2, Vector3::UnitZ()));
+    Quaternion shin_rot(Eigen::AngleAxis<Scalar>(M_PI / 8, Vector3::UnitZ()));
     robot->links_.emplace_back(
         /*parent=*/0,
-        /*joint_type=*/JointType::HINGE,
+        /*joint_type=*/JointType::FIXED,
         /*joint_pos=*/(i < 2) ? 1.0 : 0.0,
-        /*joint_rot=*/joint_rot,
+        /*joint_rot=*/leg_rot,
         /*joint_axis=*/Vector3{0.0, 1.0, 0.0},
         /*length=*/0.2);
     robot->links_.emplace_back(
-        /*parent=*/i * 2 + 1,
+        /*parent=*/i * 3 + 1,
         /*joint_type=*/JointType::HINGE,
         /*joint_pos=*/1.0,
-        /*joint_rot=*/Quaternion::Identity(),
+        /*joint_rot=*/thigh_rot,
         /*joint_axis=*/Vector3{0.0, 0.0, 1.0},
-        /*length=*/0.4);
+        /*length=*/0.2);
+    robot->links_.emplace_back(
+        /*parent=*/i * 3 + 2,
+        /*joint_type=*/JointType::HINGE,
+        /*joint_pos=*/1.0,
+        /*joint_rot=*/shin_rot,
+        /*joint_axis=*/Vector3{0.0, 0.0, 1.0},
+        /*length=*/0.2);
   }
 
   // Create a floor
@@ -80,7 +87,7 @@ int main(int argc, char **argv) {
   auto make_sim_fn = [&]() -> std::shared_ptr<Simulation> {
     std::shared_ptr<BulletSimulation> sim = std::make_shared<BulletSimulation>(time_step);
     sim->addProp(floor, Vector3{0.0, -1.0, 0.0}, Quaternion::Identity());
-    sim->addRobot(robot, Vector3{0.0, 1.0, 0.0}, Quaternion::Identity());
+    sim->addRobot(robot, Vector3{0.0, 0.425, 0.0}, Quaternion::Identity());
     return sim;
   };
 
