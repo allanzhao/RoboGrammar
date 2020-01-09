5051ea9e1544f466382e43751e418a7cd0a64aef
Allan Zhao <allanzhao@users.noreply.github.com>
2020-01-09 17:23:49 -0500

    Refactor tasks, add FrozenLakeTask
      - base task now has a forward speed objective

--- examples/design_search/tasks.py
+++ examples/design_search/tasks.py
@@ -2,20 +2,7 @@ from abc import ABC, abstractmethod
 import numpy as np
 import pyrobotdesign as rd
 
-class Task(ABC):
-  @abstractmethod
-  def add_terrain(self, sim):
-    pass
-
-  @abstractmethod
-  def get_objective_fn(self):
-    pass
-
-class RidgedTerrainTask(Task):
-  """
-  Task where the objective is to track a target velocity over ridged terrain.
-  """
-
+class ForwardSpeedTask(ABC):
   def __init__(self, time_step=1.0/240, discount_factor=0.99, interval=16,
                horizon=16, episode_len=128):
     self.time_step = time_step
@@ -24,13 +11,29 @@ class RidgedTerrainTask(Task):
     self.horizon = horizon
     self.episode_len = episode_len
 
-    self.floor = rd.Prop(0.0, 0.9, [10.0, 1.0, 10.0])
-    self.bump = rd.Prop(0.0, 0.9, [0.05, 0.10, 10.0])
-
     self.objective_fn = rd.DotProductObjective()
     self.objective_fn.base_dir_weight = np.array([-1.0, 0.0, 0.0])
     self.objective_fn.base_up_weight = np.array([0.0, 1.0, 0.0])
     self.objective_fn.base_vel_weight = np.array([2.0, 0.0, 0.0])
+
+  def get_objective_fn(self):
+    return self.objective_fn
+
+  @abstractmethod
+  def add_terrain(self, sim):
+    pass
+
+class RidgedTerrainTask(ForwardSpeedTask):
+  """
+  Task where the objective is to move forward as quickly as possible over ridged
+  terrain.
+  """
+
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+
+    self.floor = rd.Prop(0.0, 0.9, [10.0, 1.0, 10.0])
+    self.bump = rd.Prop(0.0, 0.9, [0.05, 0.10, 10.0])
 
   def add_terrain(self, sim):
     sim.add_prop(self.floor, [0.0, -1.0, 0.0],
@@ -39,5 +42,18 @@ class RidgedTerrainTask(Task):
       sim.add_prop(self.bump, [0.5 + 0.5 * i, 0.0, 0.0],
                    rd.Quaterniond(1.0, 0.0, 0.0, 0.0))
 
-  def get_objective_fn(self):
-    return self.objective_fn
+class FrozenLakeTask(ForwardSpeedTask):
+  """
+  Task where the objective is to move forward as quickly as possible on a flat,
+  low-friction surface.
+  """
+
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+
+    self.floor = rd.Prop(0.0, 0.05, [10.0, 1.0, 10.0])
+    self.floor.color = [0.8, 0.9, 1.0]
+
+  def add_terrain(self, sim):
+    sim.add_prop(self.floor, [0.0, -1.0, 0.0],
+                 rd.Quaterniond(1.0, 0.0, 0.0, 0.0))
