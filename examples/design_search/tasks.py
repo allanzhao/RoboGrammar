fbdd63ce0ae539009100a03492880294c3e1abe3
Allan Zhao <allanzhao@users.noreply.github.com>
2020-01-21 23:43:45 -0500

    Multiply rewards by 2
      - might help the design search converge faster?

--- examples/design_search/tasks.py
+++ examples/design_search/tasks.py
@@ -12,9 +12,9 @@ class ForwardSpeedTask(ABC):
     self.episode_len = episode_len
 
     self.objective_fn = rd.DotProductObjective()
-    self.objective_fn.base_dir_weight = np.array([-1.0, 0.0, 0.0])
-    self.objective_fn.base_up_weight = np.array([0.0, 1.0, 0.0])
-    self.objective_fn.base_vel_weight = np.array([1.0, 0.0, 0.0])
+    self.objective_fn.base_dir_weight = np.array([-2.0, 0.0, 0.0])
+    self.objective_fn.base_up_weight = np.array([0.0, 2.0, 0.0])
+    self.objective_fn.base_vel_weight = np.array([2.0, 0.0, 0.0])
 
     # Maximum reasonable result (designs achieving higher results are rejected)
     self.result_bound = 10.0
