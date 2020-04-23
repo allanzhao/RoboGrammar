f8e484ad13e29198ddbc96ce51b83e37a449fa33
Allan Zhao <allanzhao@users.noreply.github.com>
2020-04-23 19:33:34 -0400

    Apply random forces/torques to base link
      - not applied to MPC rollouts, only to the "ground truth" simulation
      - should penalize unstable designs

--- examples/design_search/design_search.py
+++ examples/design_search/design_search.py
@@ -115,6 +115,7 @@ def simulate(robot, task, opt_seed, thread_count, episode_count=1):
       for k in range(task.interval):
         main_sim.set_joint_target_positions(robot_idx,
                                             input_sequence[:,j].reshape(-1, 1))
+        task.add_noise(main_sim, j * task.interval + k)
         main_sim.step()
         rewards[j * task.interval + k] = objective_fn(main_sim)
     value_estimator.get_observation(main_sim, obs[:,-1])
