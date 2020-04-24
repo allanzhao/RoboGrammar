734da87720fca1d64bdac665120f212cec4e34b3
Allan Zhao <allanzhao@users.noreply.github.com>
2020-04-23 20:17:14 -0400

    Add force/torque noise parameters
      - increase force standard deviation

--- examples/design_search/tasks.py
+++ examples/design_search/tasks.py
@@ -4,13 +4,16 @@ import pyrobotdesign as rd
 
 class ForwardSpeedTask(ABC):
   def __init__(self, time_step=1.0/240, discount_factor=0.99, interval=16,
-               horizon=16, episode_len=128, noise_seed=0):
+               horizon=16, episode_len=128, noise_seed=0, force_std=10.0,
+               torque_std=1.0):
     self.time_step = time_step
     self.discount_factor = discount_factor
     self.interval = interval
     self.horizon = horizon
     self.episode_len = episode_len
     self.noise_seed = noise_seed
+    self.force_std = force_std
+    self.torque_std = torque_std
 
     self.objective_fn = rd.DotProductObjective()
     self.objective_fn.base_dir_weight = np.array([-2.0, 0.0, 0.0])
@@ -26,8 +29,9 @@ class ForwardSpeedTask(ABC):
   def add_noise(self, sim, time_step_idx):
     assert sim.get_robot_count() == 1
     noise_rng = np.random.RandomState(self.noise_seed + time_step_idx)
-    sim.add_link_force_torque(0, 0, noise_rng.normal(0.0, 1.0, size=3),
-                              noise_rng.normal(0.0, 1.0, size=3))
+    sim.add_link_force_torque(0, 0,
+                              noise_rng.normal(0.0, self.force_std, size=3),
+                              noise_rng.normal(0.0, self.torque_std, size=3))
 
   @abstractmethod
   def add_terrain(self, sim):
