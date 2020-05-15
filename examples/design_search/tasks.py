86e77136c2a76c59f6480b63860e81f161f4bbc6
eanswer <xujie7979@gmail.com>
2020-05-14 21:27:27 -0400

    add new terrains in task.py

--- examples/design_search/tasks.py
+++ examples/design_search/tasks.py
@@ -199,3 +199,103 @@ class HillTerrainTask(ForwardSpeedTask):
   def add_terrain(self, sim):
     sim.add_prop(self.heightfield, [20.0, -0.25, 0.0],
                  rd.Quaterniond(1.0, 0.0, 0.0, 0.0))
+
+'''
+higher ridges at beginning
+'''
+class NewRidgedTerrainTask(ForwardSpeedTask):
+  """
+  Task where the objective is to move forward as quickly as possible over ridged
+  terrain.
+  """
+
+  def __init__(self, seed=0, **kwargs):
+    super().__init__(**kwargs)
+    self.seed = seed
+
+    self.floor = rd.Prop(rd.PropShape.BOX, 0.0, 0.5, [20.0, 1.0, 10.0])
+    self.bump = rd.Prop(rd.PropShape.BOX, 0.0, 0.5, [0.1, 0.2, 10.0])
+
+  def add_terrain(self, sim):
+    rng = np.random.RandomState(self.seed)
+    sim.add_prop(self.floor, [0.0, -1.0, 0.0],
+                 rd.Quaterniond(1.0, 0.0, 0.0, 0.0))
+    for i in range(20):
+      sim.add_prop(self.bump,
+                   [rng.normal(0.5, 0.05) + i, -0.2 + rng.uniform(0.06, 0.22), 0.0],
+                   rd.Quaterniond(1.0, 0.0, 0.0, 0.0))
+
+'''
+wider gaps at beginning
+'''
+class NewGapTerrainTask(ForwardSpeedTask):
+  """
+  Task where the objective is to move forward as quickly as possible over
+  terrain with several large gaps.
+  """
+
+  def __init__(self, x_min=-20.0, x_max=20.0, seed=0, **kwargs):
+    super().__init__(**kwargs)
+    self.seed = seed
+
+    rng = np.random.RandomState(self.seed)
+    gap_centers = np.arange(0.5, x_max, 1.0)
+    gap_centers += rng.normal(0.0, 0.1, size=gap_centers.shape)
+    gap_widths = rng.uniform(0.2, 0.3, len(gap_centers))
+    platform_x_min = np.concatenate(([x_min], gap_centers + 0.5 * gap_widths))
+    platform_x_max = np.concatenate((gap_centers - 0.5 * gap_widths, [x_max]))
+    platform_x = 0.5 * (platform_x_min + platform_x_max)
+    platform_half_widths = 0.5 * (platform_x_max - platform_x_min)
+
+    self.platform_x = platform_x
+    self.platforms = [
+        rd.Prop(rd.PropShape.BOX, 0.0, 0.5, [half_width, 1.0, 10.0]) for
+        half_width in platform_half_widths]
+    self.floor_x = 0.5 * (x_min + x_max)
+    self.floor = rd.Prop(rd.PropShape.BOX, 0.0, 0.5,
+                         [0.5 * (x_max - x_min), 1.0, 10.0])
+
+  def add_terrain(self, sim):
+    rng = np.random.RandomState(self.seed)
+    for i, (x, platform) in enumerate(zip(self.platform_x, self.platforms)):
+      if i == 0:
+        sim.add_prop(platform, [x, -1.0, 0.0],
+                    rd.Quaterniond(1.0, 0.0, 0.0, 0.0))
+      else:
+        sim.add_prop(platform, [x, -1.0 + rng.normal(0.0, 0.01 * 5), 0.0],
+                    rd.Quaterniond(1.0, 0.0, 0.0, 0.0))
+    sim.add_prop(self.floor, [self.floor_x, -2.0, 0.0],
+                 rd.Quaterniond(1.0, 0.0, 0.0, 0.0))
+
+'''
+regular upward stairs
+'''
+class NewSteppedTerrainTask(ForwardSpeedTask):
+  """
+  Task where the objective is to move forward as quickly as possible on stepped
+  terrain.
+  """
+
+  def __init__(self, x_min=-20.0, x_max=20.0, seed=0, **kwargs):
+    super().__init__(**kwargs)
+    self.seed = seed
+
+    rng = np.random.RandomState(self.seed)
+    edge_x = np.arange(0.0, x_max, 0.3)
+    # edge_x += rng.normal(0.0, 0.1, size=edge_x.shape)
+    platform_x_min = np.concatenate(([x_min], edge_x))
+    platform_x_max = np.concatenate((edge_x, [x_max]))
+    platform_x = 0.5 * (platform_x_min + platform_x_max)
+    platform_half_widths = 0.5 * (platform_x_max - platform_x_min)
+
+    self.platform_x = platform_x
+    self.platforms = [
+        rd.Prop(rd.PropShape.BOX, 0.0, 0.5, [half_width, 5.0, 10.0]) for
+        half_width in platform_half_widths]
+
+  def add_terrain(self, sim):
+    rng = np.random.RandomState(self.seed)
+    y = -1.0
+    for i, (x, platform) in enumerate(zip(self.platform_x, self.platforms)):
+      sim.add_prop(platform, [0.5 + x, -5.0 + i * 0.1, 0.0], rd.Quaterniond(1.0, 0.0, 0.0, 0.0))
+      # y += rng.normal(0.0, min(0.015 * i, 0.1))
--- examples/graph_learning/analyze_designs.py
+++ examples/graph_learning/analyze_designs.py
@@ -11,7 +11,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--log-path', type = str, required = True)
     parser.add_argument('--grammar-file', type = str, default = '../../data/designs/grammar_apr30.dot', help="Grammar file (.dot)")
-    
+    parser.add_argument('--index', type = int, default = None, help = 'index of the designs to be shown at the end')
+
     args = parser.parse_args()
 
     fp = open(args.log_path, newline = '')
@@ -28,6 +29,8 @@ if __name__ == '__main__':
     N = 0
     best_reward = []
     rewards = []
+    rule_seqs = []
+    opt_seeds = []
     best_design = None
     best_rule_seq = None
     for row in reader:
@@ -42,48 +45,65 @@ if __name__ == '__main__':
             memory[design] = 0
         memory[design] += 1
         rule_seq = ast.literal_eval(row['rule_seq'])
+        rule_seqs.append(rule_seq)
         state = make_initial_graph()
         for rule in rule_seq:
             state = env.transite(state, rule)
         if hash(state) not in design_cnt:
-            design_cnt[hash(state)] = [0, reward]
+            design_cnt[hash(state)] = [0, reward, reward, 0]
         design_cnt[hash(state)][0] += 1
         design_cnt[hash(state)][1] = max(design_cnt[hash(state)][1], reward)
+        design_cnt[hash(state)][3] += reward
         if len(best_reward) == 0:
             best_reward = [reward]
         else:
             if reward > best_reward[-1]:
                 best_design = state
                 best_rule_seq = design
-                print('best: iter = {}, reward: {}, rule_seq: {}, hash = {}, opt_seed = {}'.format(N, reward, design, hash(state), opt_seed))
+                if design_cnt[hash(state)][0] > 1:
+                    new_design = False
+                else:
+                    new_design = True
+                if new_design:
+                    print('best: iter = {}, reward: {:.4f}, rule_seq: {}, hash = {}, opt_seed = {}, new'.format(N, reward, design, hash(state), opt_seed))
+                else:
+                    print('best: iter = {}, reward: {:.4f}, rule_seq: {}, hash = {}, opt_seed = {}, first time reward = {}'.format(N, reward, design, hash(state), opt_seed, design_cnt[hash(state)][2]))
 
             best_reward.append(max(reward, best_reward[-1]))
         rewards.append(reward)
+        opt_seeds.append(opt_seed)
 
     fp.close()
-
-    print('repeated rule sequence:')
-    repeat = 0
-    for design in memory.keys():
-        repeat += memory[design] - 1
-        if memory[design] > 10:
-            print('rule_seq = ', design, ', cnt = ', memory[design])
     
     print('repeated design')
     repeat = 0
     for key in design_cnt.keys():
         repeat += design_cnt[key][0] - 1
         if design_cnt[key][0] > 10:
-            print('reward = ', design_cnt[key][1], ', hash = ', key, ', repeated: ', design_cnt[key][0])
+            print('reward = {:.4f}'.format(design_cnt[key][1]), ', avg reward: {:.4f}'.format(design_cnt[key][3] / design_cnt[key][0]), ', hash = ', key, ', repeated: ', design_cnt[key][0])
 
     print('repeat = ', repeat, '/', N, ', ratio = ', repeat / N)
 
-    print('best rule seq = {}, best reward = {}'.format(best_rule_seq, design_cnt[hash(best_design)]))
-    
-    iters = list(range(0, len(rewards)))
-    fig, ax = plt.subplots()
-    ax.scatter(iters, rewards, s = 5, c = 'tab:blue')
-    ax.plot(iters, best_reward, c = 'tab:green')
+    print('best rule seq = {}, best reward = {:.4f}, avg reward = {:.4f}'.format(best_rule_seq, design_cnt[hash(best_design)][1], design_cnt[hash(best_design)][3] / design_cnt[hash(best_design)][0]))
+        
+    if args.index is not None:
+        print('Index {} degisn: rule_seq = {}, reward = {}, opt_seed = {}'.format(args.index, rule_seqs[args.index - 1], rewards[args.index - 1], opt_seeds[args.index - 1]))
+        split_path = args.log_path.split('/')
+        for folder in split_path:
+            if folder[-4:] == 'Task':
+                task_name = folder
+        rule_seq_str = ''
+        for i in range(len(rule_seqs[args.index - 1])):
+            if i == 0:
+                rule_seq_str = rule_seq_str + str(rule_seqs[args.index - 1][i])
+            else:
+                rule_seq_str = rule_seq_str + ', ' + str(rule_seqs[args.index - 1][i])
+        print('python examples/design_search/viewer.py {} data/designs/grammar_apr30.dot -j16 {} -o -s {} --no-noise'.format(task_name, rule_seq_str, opt_seeds[args.index - 1]))
+    else:
+        iters = list(range(0, len(rewards)))
+        fig, ax = plt.subplots()
+        ax.scatter(iters, rewards, s = 5, c = 'tab:blue')
+        ax.plot(iters, best_reward, c = 'tab:green')
 
-    plt.show()
+        plt.show()
 
